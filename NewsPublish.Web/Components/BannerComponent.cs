3bc539fedfaeffe93b149a8b58917e399299a8a0
JerryTomato <752902517@qq.com>
2018-11-17 20:59:52 +0800

    使用ViewComponent

---
+++ NewsPublish.Web/Components/BannerComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NewsPublish.Service;
+
+namespace NewsPublish.Web.Components
+{
+    public class BannerComponent: ViewComponent
+    {
+        private readonly BannerService _bannerService;
+
+        public BannerComponent(BannerService bannerService) {
+            this._bannerService = bannerService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var banners = this._bannerService.GetBannerList();
+
+            return View(banners);
+        }
+
+    }
+}
